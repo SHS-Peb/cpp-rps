427c3c8d57a7a0194195307f81e0f51a0a265416
unknown <blackskiesdancer@gmail.com>
2025-04-12 23:14:45 +1000

    logic update for rps game

--- rps.cpp
+++ rps.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
-    string userPrompt;
-    cout << "Choose rock, paper, or scissors";
-    cin >> userPrompt;
+    string userChoice;
+    string computerChoice;
+    string choices[3] = {"rock", "paper", "scissors"};
 
-    cout << "You chose: " << userPrompt << endl;
+    srand(time(0));
+
+    cout << "Choose rock, paper, or scissors: ";
+    cin >> userChoice;
+
+    int randomIndex = rand() % 3;
+    computerChoice = choices[randomIndex];
+
+    cout << "You chose: " << userChoice << endl;
+    cout << "Computer chose: " << computerChoice << endl;
+
+    if (userChoice == computerChoice) {
+        cout << "It's a draw!" << endl;
+    } else if (
+        (userChoice == "rock" && computerChoice == "scissors") ||
+        (userChoice == "scissors" && computerChoice == "paper") ||
+        (userChoice == "paper" && computerChoice == "rock")
+    ) {
+        cout << "You win!" << endl;
+    } else {
+        cout << "You lose!" << endl;
+    }
 
     return 0;
 
